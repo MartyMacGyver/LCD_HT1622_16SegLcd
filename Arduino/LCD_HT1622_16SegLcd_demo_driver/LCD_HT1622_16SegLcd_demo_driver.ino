3ca7c143d76d71110d0c026f22f297f56b861031
Martin Falatic <martin@falatic.com>
2021-10-12 01:02:21 -0700

    Many, many updates
    Resolves issue #1

--- Arduino/LCD_HT1622_16SegLcd_demo_driver/LCD_HT1622_16SegLcd_demo_driver.ino
+++ Arduino/LCD_HT1622_16SegLcd_demo_driver/LCD_HT1622_16SegLcd_demo_driver.ino
@@ -2,7 +2,7 @@
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //    Arduino demo/driver for HT1622-based 16 segment LCDs
 /*
-    Copyright (c) 2015 Martin F. Falatic
+    Copyright (c) 2015-2021 Martin F. Falatic
     
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -15,19 +15,24 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-*/
+
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-/*
+
 Board: ETM8809K2-02
 Manufacturer: http://www.canton-electronics.com
 Chipset: HT1622 or equivalent
 LCD = 9 digit 16 seg + 3 dp + 'HZ' symbol + continuous backlight
+
 Wiring:
-  VDD = 5VDC
-  K = GND
-  A = 3.3V or 5V
-  DATA, RW, CS =  5V logic, as below
-  LCD and RD not used
+  VDD  = 5VDC (3.3V doesn't seem to work)
+  K    = GND
+  VLCD = -not used-
+  DATA = 5V logic
+  RW   = 5V logic
+  RD   = -not used-
+  CS   = 5V logic
+  A    = 3.3V or 5V for backlight via 100 ohm resistor
+  K    = GND (same as other one)
 
 Arduino is little-endian
 H162x commands and addresses are MSB-first (3 bit mode + 9 bit command code)
@@ -43,11 +48,14 @@ Addressing (each address hold 4 bits):
   Addr 0x14-0x17  = Digit 1
   Addr 0x18-0x1B  = Digit 3
   Addr 0x1C-0x1F  = Digit 5
-  Addr 0x21 bit 0 = Decimal 3
-  Addr 0x21 bit 1 = Decimal 2
+  Addr 0x20       = (unused)
+  Addr 0x21 bit 3 = Decimal 3
+  Addr 0x21 bit 2 = Decimal 2
   Addr 0x22-0x25  = Digit 7
-  Addr 0x27 bit 0 = Decimal 1
-  Addr 0x27 bit 1 = Icon 'Hz'
+  Addr 0x26       = (unused)
+  Addr 0x27 bit 3 = Decimal 1
+  Addr 0x27 bit 2 = Icon 'Hz'
+  Addr 0x28+      = (unused)
 
 Typical names for segments:
 
@@ -78,97 +86,61 @@ Typical names for segments:
 Bit numbering via a little-endian uint16_t mapped to 4 addresses:
 
     /-----------\   /-----------\
-   ||     C     || ||     4     ||
+   ||     F     || ||     7     ||
     \-----------/   \-----------/
    /-\ /--\      /-\      /--\ /-\
   |   |\   \    |   |    /   /|   |
   |   | \   \   |   |   /   / |   |
-  | D |  \ E \  | 7 |  / 6 /  | 5 |
+  | E |  \ D \  | 4 |  / 5 /  | 6 |
   |   |   \   \ |   | /   /   |   |
   |   |    \   \|   |/   /    |   |
    \-/      \--/ \-/ \--/      \-/
     /-----------\   /-----------\
-   ||     F     || ||     3     ||
+   ||     C     || ||     0     ||
     \-----------/   \-----------/
    /-\      /--\ /-\ /--\      /-\
   |   |    /   /|   |\   \    |   |
   |   |   /   / |   | \   \   |   |
-  | 9 |  / A /  | B |  \ 2 \  | 1 |
+  | A |  / 9 /  | 8 |  \ 1 \  | 2 |
   |   | /   /   |   |   \   \ |   |
   |   |/   /    |   |    \   \|   |
    \-/ \--/      \-/      \--/ \-/
     /-----------\   /-----------\
-   ||     8     || ||     0     ||
+   ||     B     || ||     3     ||
     \-----------/   \-----------/
 
 Bit numbering over sequential addresses:
 
     /-----------\   /-----------\
-   ||    0-0    || ||    2-0    ||
+   ||    3-3    || ||    1-3    ||
     \-----------/   \-----------/
    /-\ /--\      /-\      /--\ /-\
   |   |\   \    |   |    /   /|   |
   |   | \   \   |   |   /   / |   |
-  |0-1|  \0-2\  |2-3|  /2-2/  |2-1|
+  |3-2|  \3-1\  |1-0|  /1-1/  |1-2|
   |   |   \   \ |   | /   /   |   |
   |   |    \   \|   |/   /    |   |
    \-/      \--/ \-/ \--/      \-/
     /-----------\   /-----------\
-   ||    0-3    || ||    3-3    ||
+   ||    3-9    || ||    0-0    ||
     \-----------/   \-----------/
    /-\      /--\ /-\ /--\      /-\
   |   |    /   /|   |\   \    |   |
   |   |   /   / |   | \   \   |   |
-  |1-1|  /1-2/  |1-3|  \3-2\  |3-1|
+  |2-2|  /2-1/  |2-0|  \0-1\  |0-2|
   |   | /   /   |   |   \   \ |   |
   |   |/   /    |   |    \   \|   |
    \-/ \--/      \-/      \--/ \-/
     /-----------\   /-----------\
-   ||    1-0    || ||    3-0    ||
+   ||    2-3    || ||    0-3    ||
     \-----------/   \-----------/
-
-Bit as int hex pos/bit values:
-
-    /-----------\   /-----------\
-   ||    1-1    || ||    3-1    ||
-    \-----------/   \-----------/
-   /-\ /--\      /-\      /--\ /-\
-  |   |\   \    |   |    /   /|   |
-  |   | \   \   |   |   /   / |   |
-  |1-2|  \1-4\  |3-8|  /3-4/  |3-2|
-  |   |   \   \ |   | /   /   |   |
-  |   |    \   \|   |/   /    |   |
-   \-/      \--/ \-/ \--/      \-/
-    /-----------\   /-----------\
-   ||    1-8    || ||    2-8    ||
-    \-----------/   \-----------/
-   /-\      /--\ /-\ /--\      /-\
-  |   |    /   /|   |\   \    |   |
-  |   |   /   / |   | \   \   |   |
-  |0-2|  /0-4/  |0-8|  \2-4\  |2-2|
-  |   | /   /   |   |   \   \ |   |
-  |   |/   /    |   |    \   \|   |
-   \-/ \--/      \-/      \--/ \-/
-    /-----------\   /-----------\
-   ||    0-1    || ||    2-1    ||
-    \-----------/   \-----------/
-
 
 */
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Helper functions and variables
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 #define ARRAY_SIZE(x) ((sizeof x) / (sizeof *x))
-
-// Function that printf and related will use to print
-int serial_putchar(char c, FILE* f) {
-  if (c == '\n') serial_putchar('\r', f);
-  return Serial.write(c) == 1 ? 0 : 1;
-}
-
-FILE serial_stdout;
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // HT162x commands (Start with 0b100, ends with one "don't care" byte)
@@ -176,35 +148,47 @@ FILE serial_stdout;
 #define  CMD_SYS_EN   0x01  // SYS EN     (0000-0001-X) Turn on  system oscillator
 #define  CMD_LCD_OFF  0x02  // LCD OFF    (0000-0010-X) Turn off LCD display [Default]
 #define  CMD_LCD_ON   0x03  // LCD ON     (0000-0011-X) Turn on  LCD display
-#define  CMD_RC_INT   0x10  // RC INT     (0001-10XX-X) System clock source, on-chip RC oscillator
+#define  CMD_RC_INT   0x18  // RC INT     (0001-10XX-X) System clock source, on-chip RC oscillator
 #define  CMD_BIAS_COM 0x29  // BIAS & COM (0010-10X1-X) 1/3 bias, 4 commons // HT1621 only
 
-#define CS   8 // Active low
-#define WR   9 // Active low
-#define DATA 10
+#define CS   27 // Active low
+#define WR   15 // Active low
+#define DATA  4
 
-#define MSB_FORMAT  false
 #define LSB_FORMAT  true
+#define MSB_FORMAT  false
+
+#define SETUP_DELAY_USECS 1
+#define HOLD_DELAY_USECS  1
 #define WRITE_DELAY_USECS 2  // Tclk min. on data sheet - overhead is more than this at low clock speeds
+#define RESET_DELAY_USECS 1000  // Not strictly necessary
 
 #define NUM_DIGITS  9
 
-const uint8_t digitAddr[] = {0x00, 0x14, 0x04, 0x18, 0x08, 0x1C, 0x0C, 0x22, 0x10};
+const uint8_t digitAddr[NUM_DIGITS] = {
+  0x00, 0x14, 0x04, 0x18, 0x08, 0x1C, 0x0C, 0x22, 0x10,
+};
+
+// TODO
+//boolean VAL_DP1 = false;
+//boolean VAL_DP2 = false;
+//boolean VAL_DP3 = false;
+//boolean VAL_HZ  = false;
 
 // Based on a font presented at http://windways.org/personal_page/stockticker/
-const uint16_t SegCharData[] = {
-  0x0000, 0x0023, 0x00A0, 0xAA88, 0xB99B, 0xBCCB, 0xD385, 0x0080, //  !"#$%&'
-  0x0044, 0x4400, 0xCCCC, 0x8888, 0x0040, 0x8008, 0x0001, 0x0440, // ()*+,-./
-  0x3773, 0x0062, 0x9339, 0x113b, 0xA02A, 0xB11B, 0xB30B, 0x1032, // 01234567
-  0xb33b, 0xb03b, 0x0880, 0x0480, 0x8044, 0x8109, 0x4408, 0x1838, // 89:;<=>?
-  0x3379, 0xB23A, 0x19BB, 0x3311, 0x19B3, 0xB311, 0xB210, 0x331B, // @ABCDEFG
-  0xA22A, 0x1991, 0x0323, 0xA244, 0x2301, 0x6262, 0x6226, 0x3333, // HIJKLMNO
-  0xB238, 0x3337, 0xB23C, 0xB11B, 0x1890, 0x2323, 0x2640, 0x2626, // PQRSTUVW
-  0x4444, 0xA828, 0x1551, 0x0891, 0x4004, 0x1980, 0x0404, 0x0101, // XYZ[\]^_
-  0x4000, 0xCB00, 0xAB00, 0x8300, 0x8B80, 0x8701, 0x8898, 0xB980, // `abcdefg
-  0xAA00, 0x0800, 0x0B80, 0x08C4, 0x0880, 0x8A0A, 0x8A00, 0x8B00, // hijklmno
-  0xB280, 0xB880, 0x8200, 0xB900, 0x8888, 0x0B00, 0x0600, 0x0606, // pqrstuvw
-  0x4444, 0x4840, 0x8500, 0x8891, 0x2200, 0x1988, 0x4040, 0xFFFF, // xyz{|}~
+const uint16_t SegCharDataLSBfirst[] = {
+  0x0000, 0x004C, 0x0050, 0x5511, 0xD99D, 0xD33D, 0xBC1A, 0x0010, //  !"#$%&'
+  0x0022, 0x2200, 0x3333, 0x1111, 0x0020, 0x1001, 0x0008, 0x0220, // ()*+,-./
+  0xCEEC, 0x0064, 0x9CC9, 0x88CD, 0x5045, 0xD88D, 0xDC0D, 0x80C4, // 01234567
+  0xDCCD, 0xD0CD, 0x0110, 0x0210, 0x1022, 0x1809, 0x2201, 0x81C1, // 89:;<=>?
+  0xCCE9, 0xD4C5, 0x89DD, 0xCC88, 0x89DC, 0xDC88, 0xD480, 0xCC8D, // @ABCDEFG
+  0x5445, 0x8998, 0x0C4C, 0x5422, 0x4C08, 0x6464, 0x6446, 0xCCCC, // HIJKLMNO
+  0xD4C1, 0xCCCE, 0xD4C3, 0xD88D, 0x8190, 0x4C4C, 0x4620, 0x4646, // PQRSTUVW
+  0x2222, 0x5141, 0x8AA8, 0x0198, 0x2002, 0x8910, 0x0202, 0x0808, // XYZ[\]^_
+  0x2000, 0x3D00, 0x5D00, 0x1C00, 0x1D10, 0x1E08, 0x1191, 0xD910, // `abcdefg
+  0x5500, 0x0100, 0x0D10, 0x0132, 0x0110, 0x1505, 0x1500, 0x1D00, // hijklmno
+  0xD410, 0xD110, 0x1400, 0xD900, 0x1111, 0x0D00, 0x0600, 0x0606, // pqrstuvw
+  0x2222, 0x2120, 0x1A00, 0x1198, 0x4400, 0x8911, 0x2020, 0xFFFF, // xyz{|}~
 };
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -212,129 +196,137 @@ const uint16_t SegCharData[] = {
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 void HT162x_SendBits(uint16_t data, uint8_t bits, boolean LSB_FIRST = MSB_FORMAT)
 {
-  uint16_t mask;
-  mask = (LSB_FIRST ? 1 : 1 << bits-1 );
-  //printf("Writing %d bits of 0x%04x with mask 0x%04x in %s\n", bits, data, mask, LSB_FIRST?"LSB":"MSB");
+  // Data is shifted out bitwise, either LSB-first or MSB-first.
+  // The mask is used to isolate the bit being transmitted.
+
+  uint16_t mask = LSB_FIRST ? 1 : 1 << bits-1;
 
   for (uint8_t i = bits; i > 0; i--)
   {
+    delayMicroseconds(WRITE_DELAY_USECS);
     digitalWrite(WR, LOW);
-    //delayMicroseconds(WRITE_DELAY_USECS);
     data & mask ? digitalWrite(DATA, HIGH) : digitalWrite(DATA, LOW);
     delayMicroseconds(WRITE_DELAY_USECS);
     digitalWrite(WR, HIGH);
-    delayMicroseconds(WRITE_DELAY_USECS);
+    delayMicroseconds(HOLD_DELAY_USECS);
     LSB_FIRST ? data >>= 1 : data <<= 1;
   }
-  delayMicroseconds(WRITE_DELAY_USECS);
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 void HT162x_Command(uint8_t cmd)
 {
+  delayMicroseconds(SETUP_DELAY_USECS);
   digitalWrite(CS, LOW);
+  delayMicroseconds(SETUP_DELAY_USECS);
   HT162x_SendBits(0b100, 3);
   HT162x_SendBits(cmd, 8);
   HT162x_SendBits(1, 1);
+  delayMicroseconds(SETUP_DELAY_USECS);
   digitalWrite(CS, HIGH);
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 void HT162x_WriteData(uint8_t addr, uint16_t sdata, uint8_t bits = 4)
 {
+  // Note: bits needs to be a multiple of 4 as data is in nibbles
+  delayMicroseconds(SETUP_DELAY_USECS);
   digitalWrite(CS, LOW);
+  delayMicroseconds(SETUP_DELAY_USECS);
   HT162x_SendBits(0b101, 3);
   HT162x_SendBits(addr, 6);
-  HT162x_SendBits(sdata, bits, MSB_FORMAT);
+  for (int n = (bits/4)-1; n >= 0; n--) {
+    uint16_t nib = (sdata & (0xf) << 4*n) >> (4*n);
+    HT162x_SendBits(nib, 4, LSB_FORMAT);
+  }
+  delayMicroseconds(SETUP_DELAY_USECS);
   digitalWrite(CS, HIGH);
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-void TestSegments(uint8_t addr)
+void AllElements(uint8_t state)
 {
-    const int SegDelay = 50;
-    
-    // Test cycle (single uint16_t writes)
-    for (uint8_t i = 0x00; i <= 0x1F; i++) {
-      HT162x_WriteData(addr, 1<<i, 16);
-      delay(SegDelay);
-    }
-    HT162x_WriteData(addr, 0, 16);
-    delay(SegDelay);
-    
-    // Test cycle (sequential nibbles)
-    for (uint8_t j = 0; j < 4; j++) {
-      for (uint8_t i = 0; i < 4; i++) {
-        HT162x_WriteData(addr+j, 1<<i, 4);
-        delay(SegDelay);
-      }
-      HT162x_WriteData(addr, 0, 16);
-    }
-    HT162x_WriteData(addr, 0, 16);
-    delay(SegDelay);
+  for (uint8_t addr = 0; addr <= 0x27; addr++)
+  {
+    HT162x_WriteData(addr, (state ? 0xf : 0x0), 4);
+  }
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 void AllSegments(uint8_t state)
 {
-  const int SegDelay = 3000;
- 
-  for (uint8_t addr = 0x00; addr < 0x3F; addr++)
+  for (uint8_t pos = 0; pos < NUM_DIGITS; pos++)
   {
-    HT162x_WriteData(addr, (state ? 0xff : 0x00));
+    HT162x_WriteData(digitAddr[pos], (state ? 0xffff : 0x0000), 16);
   }
-  delay(SegDelay);
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-void TestChars()
+void RandomElements(int segDelay = 1, int cycles = 9)
 {
-  const int CharDelay = 50;
+  for (int cnt = 0; cnt < cycles; cnt++)
+  {
+    uint8_t addr = random(0x27+1);
+    uint16_t sdata = random(0xF+1);
+    HT162x_WriteData(addr, sdata, 4);
+    delay(segDelay);
+  }
+}
 
-  int aSize = ARRAY_SIZE(SegCharData);
-  printf("Asize = %d\n", aSize);
-  int charData = 0;
-  for (int n = 0; n < 10000; n++)
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+void RandomSegments(int segDelay = 1, int cycles = 1)
+{
+  for (int cnt = 0; cnt < cycles; cnt++)
+  {
+    for (int n = 0; n < NUM_DIGITS; n++)
+    {
+      uint8_t addr = digitAddr[n] + random(4);
+      uint16_t sdata = random(0xF+1);
+      HT162x_WriteData(addr, sdata, 4);
+      delay(segDelay);
+    }
+  }
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+void TestSegments(int segDelay = 200, boolean clear_as = true, int cycles = 1)
+{
+  for (int cnt = 0; cnt < cycles; cnt++)
   {
     for (uint8_t pos = 0; pos < NUM_DIGITS; pos++)
     {
-      HT162x_WriteData(digitAddr[pos], SegCharData[charData], 16);
-      delay(CharDelay);
-      charData = (charData < aSize-1 ? charData+1 : 0);
+      // Test cycle (single uint16_t writes)
+      uint16_t sdata = 0;
+      for (uint8_t i = 0x00; i < 16; i++) {
+        sdata = clear_as ? 1 << i: sdata | (1 << i);
+        HT162x_WriteData(digitAddr[pos], sdata, 16);
+        delay(segDelay);
+      }
+      if (clear_as) {
+        HT162x_WriteData(digitAddr[pos], 0, 16);
+      }
     }
   }
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-void RandomSegments(uint16_t cycles)
+void TestChars(int CharDelay = 150, int cycles = 1)
 {
-    for (int j = 0; j < cycles; j++)
-    {
-      for (int n = 0; n < NUM_DIGITS; n++)
-      {
-        uint16_t sdata = random(0x10);
-        uint16_t ndigit = random(NUM_DIGITS);
-        uint8_t addr = digitAddr[n] + random(4);
-        HT162x_WriteData(addr, sdata);
-      }
-      delay(1);
-    }
-}
+  int aSize = ARRAY_SIZE(SegCharDataLSBfirst);
 
-//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-union QuadUnion {
-  uint32_t x32;
-  struct {
-    uint16_t lo;
-    uint16_t hi;
-  } x16;
-  struct {
-    uint8_t lo0;
-    uint8_t hi0;
-    uint8_t lo1;
-    uint8_t hi1;
-  } x8;
-};
+  for (int cnt = 0; cnt < cycles; cnt++)
+  {
+    int charData = 0;
+    for (int n = 0; n < aSize / NUM_DIGITS * 2; n++)
+    {
+      for (uint8_t pos = 0; pos < NUM_DIGITS; pos++)
+      {
+        HT162x_WriteData(digitAddr[pos], SegCharDataLSBfirst[charData], 16);
+        delay(CharDelay);
+        charData = (charData < aSize-1 ? charData+1 : 0);
+      }
+    }
+  }
+}
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 //#############################################################################
@@ -342,17 +334,7 @@ union QuadUnion {
 void setup()
 {
   Serial.begin(115200);
-
-  // Set up stdout for printf() use
-  fdev_setup_stream(&serial_stdout, serial_putchar, NULL, _FDEV_SETUP_WRITE);
-  stdout = &serial_stdout;
-  //printf("My favorite number is %6d!\n", 42);
-
-  QuadUnion foo;
-  foo.x32 = 0xaa55ff00; //0b10101010010101011111111100000000; //0xaa55ff00;
-  printf("Foo = 0x%08x\n", foo.x32);
-  printf("Foo = 0x%04x,0x%04x\n", foo.x16.lo, foo.x16.hi);
-  printf("Foo = 0x%02x,0x%02x,0x%02x,0x%02x\n", foo.x8.lo0, foo.x8.hi0, foo.x8.lo1, foo.x8.hi1);
+  delayMicroseconds(RESET_DELAY_USECS);
 
   // Set up I/O and display
   pinMode(CS, OUTPUT);
@@ -364,11 +346,9 @@ void setup()
   HT162x_Command(CMD_RC_INT);
   //HT162x_Command(CMD_BIAS_COM); // Only for HT1621
   HT162x_Command(CMD_LCD_OFF);
-  AllSegments(1);
+  AllElements(0);
   HT162x_Command(CMD_LCD_ON); // Should turn it back on
   delay(1000);
-  AllSegments(0);
-  delay(500);
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -376,17 +356,29 @@ void setup()
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 void loop()
 {
-  for (uint8_t pos = 0; pos < NUM_DIGITS; pos++)
-  {
-    TestSegments(digitAddr[pos]);
-  }
-
-  RandomSegments(1000);
-
-  //TestChars();
-
+  AllElements(1);
   delay(1000);
+  AllElements(0);
+  delay(1000);
+  AllSegments(1);
+  delay(1000);
+  AllSegments(0);
+  delay(1000);
+
+  RandomElements(10, 900);
+  AllElements(0);
+  
+  RandomSegments(10, 100);
+  AllElements(0);
+  
+  TestSegments(25, false, 1);
+  AllElements(0);
+  
+  TestSegments(100, true, 1);
+  AllElements(0);
+  
+  TestChars(50, 1);
+  AllElements(0);
 }
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # LCD_HT1622_16SegLcd
+
 Experiments with a 9-character 16-segment alphanumeric LCD
 
 Board: ETM8809K2-02
@@ -6,6 +7,9 @@ Board: ETM8809K2-02
 Manufacturer: http://www.canton-electronics.com
 
 Chipset: HT1622 or equivalent
+
+Datasheets: ([HT1621](https://www.holtek.com/documents/10179/116711/HT1621v321.pdf)
+and [HT1622](https://www.holtek.com/documents/10179/116711/HT1622v270.pdf) datasheets)
 
 LCD = 9 digit 16 seg + 3 dp + 'HZ' symbol + continuous backlight
 
